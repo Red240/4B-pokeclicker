88e1cb707a83b9d03244e9b22b2beeef1d7955a6
Damian Dhesi <mac8@129mac18.valleychristian.net>
2017-01-23 14:49:49 -0800

    i did the javascript for the clicker i think
    i need to get a photo to check though

--- webapp.js
+++ webapp.js
@@ -1,1 +1,11 @@
 // JavaScript Document
+// This is the cookie clicker and it is suppose to be able to count the cookies
+
+var pokeball=0;
+
+function poke-clicker() {
+	
+	pokeball++;
+	document.getElementById("output").innerHTML = cookies;
+	
+	}
