2eac755b8889fe88ea4b893db3874a5f7ee7cea9
Damian Dhesi <mac8@129mac18.valleychristian.net>
2017-01-25 14:58:07 -0800

    fixed my code to be fine with the pull request

--- webapp.js
+++ webapp.js
@@ -1,17 +1,6 @@
 // JavaScript Document
-<<<<<<< HEAD
-// This is the cookie clicker and it is suppose to be able to count the cookies
 
-var pokeball=0;
-
-function poke_clicker() {
-	pokeball++;
-	document.getElementById("output").innerHTML = pokeball;
-	
-	}
-=======
 var i = 0;
 function buttonClick() {
 	document.getElementById('inc').value = ++i;
 }
->>>>>>> Version-1.0
