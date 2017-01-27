c7ab4dc209135522bdf888d1533722945ff29fba
Damian Dhesi <mac8@129mac18.valleychristian.net>
2017-01-27 14:50:32 -0800

    worked on the shop and set interval a bit

--- webapp.js
+++ webapp.js
@@ -1,13 +1,20 @@
 // JavaScript Document
 
+//This function below which is the buttonclick makes it so that the pokeclicker works and is spamable. It
 var i = 0;
 function buttonClick() {
 	document.getElementById('inc').value = ++i;
 }
 
-var caterpie =0;
+var caterpie = 0;
 function firstclick() {
 	caterpie++;
 	
+	
+}
+
+if(caterpie >= 1) {
+	
+setInterval()	
 	
 	}
