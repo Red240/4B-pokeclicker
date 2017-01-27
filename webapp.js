857c346ffcf684605519eeb41bc5f3e3a4d4d8b4
Damian Dhesi <mac8@129mac18.valleychristian.net>
2017-01-27 14:51:11 -0800

    now i worked on the shop

--- webapp.js
+++ webapp.js
@@ -2,16 +2,26 @@
 
 //This function below which is the buttonclick makes it so that the pokeclicker works and is spamable. It
 var i = 0;
+var pikachu = 0;
+var caterpie = 0;
+var charmander = 0;
+
 function buttonClick() {
 	document.getElementById('inc').value = ++i;
 }
 
-var caterpie = 0;
-function firstclick() {
-	caterpie++;
-	
-	
+function shop(pokemon) {
+	switch(pokemon) {
+		case "pikachu":
+			if(i>=40) {		
+				pikachu++;
+				i-=40;
+			}
+		
+	}
 }
+
+
 
 if(caterpie >= 1) {
 	
