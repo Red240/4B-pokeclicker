28f978c8e90d726f679a0685f0df1f8cd2b9c8b2
Damian Dhesi <mac8@129mac18.valleychristian.net>
2017-02-09 13:52:20 -0800

    testing math.round

--- webapp.js
+++ webapp.js
@@ -12,7 +12,7 @@ var Drantini = 0;
 var Arceus = 0;
 
 function update() {
-	document.getElementById("numberofpokeballs").value = pokeballs;
+	document.getElementById("numberofpokeballs").value = Math.round(pokeballs, -1);
 }
 
 
